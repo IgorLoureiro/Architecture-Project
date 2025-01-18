3cc22871aa41388d7fadf12607c932e9f8e08cec
IgorL <igor.loureiro@mobilus.com.br>
2025-01-18 18:38:42 -0300

    First commit of the project, created a middleware, controller, useCases, Requests and text Exceptions

---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -0,0 +1,44 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Application.UseCases.User.Register
+{
+    public class RegisterUserUseCase
+    {
+        public ResponseRegisteredUserJson Execute(RequestRegisterUserJson request)
+        {
+
+            Validate(request);
+
+            //Validar a request
+
+            //mapear uma request em uma entidade
+
+            //Salvar no banco de dados
+
+            //Criptografia da senha
+
+            return new ResponseRegisteredUserJson
+            {
+                Name = request.Name,
+            };
+        }
+
+        private void Validate(RequestRegisterUserJson request)
+        {
+            var validator = new RegisterUserValidator();
+            var result = validator.Validate(request);
+
+            if (result.IsValid == false) { 
+                var errorMessages = result.Errors.Select(e => e.ErrorMessage);
+
+                throw new Exception();
+            }
+        }
+    }
+}   
